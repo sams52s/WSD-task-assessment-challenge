e3092374152b5f4919410afbdb106dee5b62273f
sams52s <sams52tas@gmail.com>
2025-05-01 16:23:12 +0600

    Added base files and test structure from provided case study

---
+++ wsd-certificate-feed-generator/src/main/java/org/wsd/generator/App.java
@@ -0,0 +1,14 @@
+package org.wsd.generator;
+
+public class App {
+    public static void main(String[] args) {
+        if (args.length >= 2) {
+            int threads = Integer.parseInt(args[0]);
+            int quotes = Integer.parseInt(args[1]);
+
+            CertificateUpdateGenerator certificateUpdateGenerator = new CertificateUpdateGenerator(threads, quotes);
+            certificateUpdateGenerator.generateQuotes();
+        }
+        throw new RuntimeException("Expect at least number of threads and number of quotes. But got: " + args);
+    }
+}
